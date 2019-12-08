151ba2d454757b64ff8bedd60c17b85d6a2356df
joshuabeaver_ <47809705+imnotbeavs@users.noreply.github.com>
2019-12-08 22:56:10 +0800

    Update Python Prob3.py

--- Python/Problem 3/Python Prob3.py
+++ Python/Problem 3/Python Prob3.py
@@ -1,6 +1,10 @@
+
 import matplotlib.pyplot as plt
 import numpy as np
 import math as m
+print('Sample input  MP_3(np.array([[1,2],[3,4],...))')
+print('')
+print('Wherein 1 and 3 are the  are the x-components and \n2 and 4 are the y- components of \nthe data points')
 def MP_3(comp):
     x = comp[:,0]
     y = comp[:,1]
